9973e2a0d689eec2dfdff4f9ac8b17ba187b0739
Riza Khan <riza.khan@appnovation.com>
2021-09-05 00:32:25 -0400

    More styling added and upgraded node-sass to prevent netlify build failures

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <router-view />
+    <div class="app__title">
+      <h3>Godmode</h3>
+    </div>
+    <div class="app__body">
+      <router-view />
+    </div>
+    <div class="app__footer">
+      <p>Footer Content goes here</p>
+    </div>
   </div>
 </template>
 
@@ -13,9 +21,42 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-size: 16px;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+h3 {
+  font-size: 2rem;
+}
+
+p {
+  font-size: 1rem;
 }
 
 body {
   font-family: "Roboto", sans-serif;
+
+  .app {
+    display: flex;
+    grid-gap: 1rem;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 100vh;
+
+    &__title {
+      border-bottom: solid 1px black;
+    }
+
+    &__body {
+      margin-bottom: auto;
+    }
+
+    &__footer {
+      border-top: solid 1px black;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="content">
     <h1>Content goes here</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, fugit
+      nulla odit quae aliquid doloribus exercitationem quo reprehenderit
+      laudantium ut voluptatibus quasi vitae nisi voluptates laborum sequi,
+      impedit, rem adipisci? Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. Nemo est labore rerum sunt consequuntur. Tenetur quibusdam est
+      temporibus, iusto, optio ipsam similique nihil veniam minus libero
+      distinctio ex facere vero. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Optio ratione quidem praesentium dolores quos? Aliquam
+      adipisci numquam ea libero distinctio nisi odit voluptatibus esse tempora
+      fugit. Eveniet magni mollitia culpa!
+    </p>
   </div>
 </template>
 
